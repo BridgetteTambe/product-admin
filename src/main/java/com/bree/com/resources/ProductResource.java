e101d982c0489bc46ea384bc21ded11f7c8e3f47
BridgetteTambe <bridgettetambe@gmail.com>
2024-02-23 08:52:53 +0200

    worked on products added the endpoints 1

--- src/main/java/com/bree/com/resources/ProductResource.java
+++ src/main/java/com/bree/com/resources/ProductResource.java
@@ -42,6 +42,7 @@ public class ProductResource {
         if(product.getId() == null){
             throw new Exception("Cant update a product with a null Id.");
         }
+
         Product save = this.productService.save(product);
         return ResponseEntity.ok(save);
     }
