96e7c8c54077d0267a4e2e771425aeecbafe65bf
BridgetteTambe <bridgettetambe@gmail.com>
2024-02-23 08:00:03 +0200

    worked on products added the endpoints

---
+++ src/main/java/com/bree/com/exceptions/dto/ExceptionResponse.java
@@ -0,0 +1,9 @@
+package com.bree.com.exceptions.dto;
+
+public class ExceptionResponse {
+    private String message;
+
+    public ExceptionResponse(String message) {
+        this.message = message;
+    }
+}
